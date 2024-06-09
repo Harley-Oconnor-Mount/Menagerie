b4838d923cc21716a379bd762aa5c0744fe6b11c
harley <harley.oconnormont@ec.gc.ca>
2024-06-09 15:35:18 -0400

    First commit

---
+++ menagerie.R
@@ -0,0 +1,7 @@
+if(require(c("ape", "phangorn", "phylotools", "ggplot2", "gplots", "spider". "rentrez")) == FALSE)
+    install.packages(c("ape", "phangorn", "phylotools", "ggplot2", "gplots", "spider". "rentrez"), 
+        repos = "http://cran.us.r-project.org")
+
+
+
+library(rentrez)
